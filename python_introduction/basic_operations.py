ee84796590008f156d4d5c9d480ec8e6e76f6e79
xSSanDev <xssanass@gmail.com>
2024-09-11 10:53:18 +0000

    Adding repo + file

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+sum = number1 + number2
+print(f"Addition of {number1} and {number2} is {sum}")
+difference = number1 - number2
+print(f"Subtraction of {number1} and {number2} is {difference}")
+product = number1 * number2
+print(f"Multiplication of {number1} and {number2} is {product}")
