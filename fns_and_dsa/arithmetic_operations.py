1050b4530550655d91a2940ead45ab0014be0bc4
xSSanDev <xssanass@gmail.com>
2024-09-29 08:14:01 -0700

    adding  operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,7 +1,7 @@
 def perform_operation(num1, num2, operation):
     if operation == 'add':
         return num1 + num2
-    elif operation == 'substract':
+    elif operation == 'subtract':
         return num1 - num2
     elif operation == 'multiply':
         return num1 * num2
