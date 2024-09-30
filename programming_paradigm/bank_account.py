dda7feb60133c5942e42595251b23ce3110dd7e4
xSSanDev <xssanass@gmail.com>
2024-09-30 12:12:23 +0000

    oop.

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -13,4 +13,5 @@ class BankAccount:
             return False
         
     def display_balance(self):
-        print(f"Current balance: &{self.account_balance:.2f}")
+        print(f"Current Balance: &{self.account_balance:.2f}")
+
