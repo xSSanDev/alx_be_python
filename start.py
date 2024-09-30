dae9c13a87b07649c6975ed7b3cfcd8dbce5439f
xSSanDev <xssanass@gmail.com>
2024-09-30 14:33:46 +0000

    .

--- start.py
+++
@@ -1,8 +0,0 @@
-import os
-
-my_folder = os.getcwd()
-print(f"Here are the files in {my_folder}:")
-
-with os.scandir(my_folder) as folder:
-    for entry in folder:
-        print(f" - {entry.name}")
