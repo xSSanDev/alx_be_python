ad20e2c6e25a12d3076a17b56b412a1d9d827414
xSSanDev <xssanass@gmail.com>
2024-10-03 11:47:00 +0000

    .

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,31 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self._is_checked_out = False
+
+class Library:
+    def __init__(self):
+        self._books = []
+
+    def add_book(self, book):
+        self._books.append(book)
+
+    def check_out_book(self, title):
+        for book in self._books:
+            if book.title == title and not book._is_checked_out:
+                book._is_checked_out = True
+                return True
+        return False
+
+    def return_book(self, title):
+        for book in self._books:
+            if book.title == title and book._is_checked_out:
+                book._is_checked_out = False
+                return True
+        return False
+
+    def list_available_books(self):
+        for book in self._books:
+            if not book._is_checked_out:
+                print(f"{book.title} by {book.author}")
